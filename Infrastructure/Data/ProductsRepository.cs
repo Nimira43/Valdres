c7a65e52668b29d01395ae2f409334dc213db9e3
Nimira43 <n.rabone72@aol.com>
2025-12-30 18:34:08 +0000

    Implemented the interface

--- Infrastructure/Data/ProductsRepository.cs
+++ Infrastructure/Data/ProductsRepository.cs
@@ -1,6 +1,42 @@
+using Core.Entities;
+using Core.Interfaces;
+
 namespace Infrastructure.Data;
 
-public class ProductsRepository
+public class ProductsRepository : IProductRepository
 {
+  public void AddProduct(Product product)
+  {
+    throw new NotImplementedException();
+  }
 
+  public void DeleteProduct(Product product)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Product?> GetProductByIdAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IReadOnlyList<Product>> GetProductsAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool ProductExists(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<bool> SaveChangesAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void UpdateProduct(Product product)
+  {
+    throw new NotImplementedException();
+  }
 }
