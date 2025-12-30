d4b6f5bc58f0850174940da8887d7d8f513ba4e5
Nimira43 <n.rabone72@aol.com>
2025-12-30 15:15:22 +0000

    ProductConfiguration - 1

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -6,4 +6,12 @@ namespace Infrastructure.Data;
 public class StoreContext(DbContextOptions options) : DbContext(options)
 {
   public DbSet<Product> Products { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Product>()
+        .Property(p => p.Price)
+        .HasPrecision(18, 2);
+  }
+
 }
