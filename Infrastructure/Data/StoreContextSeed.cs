d60bfa7955a6f7d011890b7a7c37b42af60f6e54
Nimira43 <n.rabone72@aol.com>
2025-12-30 19:49:12 +0000

    StoreContextSeed - 2

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -4,6 +4,9 @@ public class StoreContextSeed
 {
   public static async Task SeedAsync(StoreContext context)
   {
-    
+    if (!context.Products.Any())
+    {
+      var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+    }
   }
 }
