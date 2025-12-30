61b158c865eaf7135771265ac24ee101725ebe8f
Nimira43 <n.rabone72@aol.com>
2025-12-30 15:17:40 +0000

    ProductConfiguration - 2

--- Infrastructure/Config/ProductConfiguration.cs
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -8,6 +8,6 @@ public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
   public void Configure(EntityTypeBuilder<Product> builder)
   {
-    throw new NotImplementedException();
+    builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
   }
 }
