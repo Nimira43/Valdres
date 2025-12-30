66abce7c151032eabd17aa150e324c5ab1881a2a
Nimira43 <n.rabone72@aol.com>
2025-12-30 18:27:41 +0000

    IProductRepository - 2

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -2,8 +2,13 @@ using Core.Entities;
 
 namespace Core.Interfaces;
 
-public class IProductRepository
+public interface IProductRepository
 {
   Task<IReadOnlyList<Product>> GetProductsAsync();
   Task<Product?> GetProductByIdAsync(int id);
+  void AddProduct(Product product);
+  void UpdateProduct(Product product);
+  void DeleteProduct(Product product);
+  bool ProductExists(int id);
+  Task<bool> SaveChangesAsync();
 }
