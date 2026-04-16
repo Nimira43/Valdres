b682f9c8304760491609def201ae2191bd3add80
Nimira43 <n.rabone72@aol.com>
2026-04-16 23:52:27 +0100

    Updating Angular files

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
+import 'zone.js'
 
 import { routes } from './app.routes'
 
