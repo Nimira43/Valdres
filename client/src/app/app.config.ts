47a399f401d8c8444d004b7f3c3534dab32386f6
Nimira43 <n.rabone72@aol.com>
2026-05-02 03:07:52 +0100

    Bringing list of product name using http request.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router'
 import 'zone.js'
 
 import { routes } from './app.routes'
+import { provideHttpClient } from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient()
   ]
 }
