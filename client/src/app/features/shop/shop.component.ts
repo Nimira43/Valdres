f722f3c53caa93976febf415815f158ef26b8b4a
Nimira43 <n.rabone72@aol.com>
2026-05-28 17:26:10 +0100

    ShopParams class.

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -8,13 +8,13 @@ import { MatIcon } from '@angular/material/icon'
 import { MatCard } from '@angular/material/card'
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu'
 import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list'
+import { ShopParams } from '../../shared/models/shopParams'
 
 @Component({
   selector: 'app-shop',
   imports: [
     ProductItemComponent,
     MatIcon,
-    MatCard,
     MatMenu,
     MatSelectionList,
     MatListOption,
@@ -27,14 +27,12 @@ export class ShopComponent implements OnInit{
   private shopService = inject(ShopService)
   private dialogService = inject(MatDialog)
   products: Product[] = []
-  selectedBrands: string[] = []
-  selectedTypes: string[] = []
-  selectedSort: string = 'name'
   sortOptions = [
     {name: 'Alphabetical', value: 'name'},
     {name: 'Price: Low-High', value: 'priceAsc'},
     {name: 'Price: High-Low', value: 'priceDesc'},
   ]
+  shopParams = new ShopParams()
 
   ngOnInit(): void {
     this.initialiseShop()      
@@ -47,21 +45,19 @@ export class ShopComponent implements OnInit{
   }
 
   getProducts() {
-    this.shopService.getProducts(
-      this.selectedBrands,
-      this.selectedTypes,
-      this.selectedSort
-    ).subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-    })
+    this.shopService
+      .getProducts(this.shopParams)
+      .subscribe({
+        next: response => this.products = response.data,
+        error: error => console.log(error),
+      })
   }
 
   onSortChange(event: MatSelectionListChange) {
     const selectedOption = event.options[0]
  
     if (selectedOption) {
-      this.selectedSort = selectedOption.value
+      this.shopParams.sort = selectedOption.value
       this.getProducts()
     }
   }
@@ -70,15 +66,15 @@ export class ShopComponent implements OnInit{
     const dialogRef = this.dialogService.open(FiltersDialogComponent, {
       minWidth: '500px',
       data: {
-        selectedBrands: this.selectedBrands,
-        selectedTypes: this.selectedTypes,
+        selectedBrands: this.shopParams.brands,
+        selectedTypes: this.shopParams.types,
       }
     })
     dialogRef.afterClosed().subscribe({
       next: result => {
         if (result) {
-          this.selectedBrands = result.selectedBrands
-          this.selectedTypes = result.selectedTypes
+          this.shopParams.brands = result.selectedBrands
+          this.shopParams.types = result.selectedTypes
           this.getProducts()
         }
       }
