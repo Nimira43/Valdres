2e6b51fc4213045406ce1d574e9ae95bf0e30904
Nimira43 <n.rabone72@aol.com>
2026-07-05 12:15:49 +0100

    TestErrorComponent

---
+++ client/src/app/features/test-error/test-error.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-test-error',
+  imports: [],
+  templateUrl: './test-error.component.html',
+  styleUrl: './test-error.component.css',
+})
+export class TestErrorComponent {
+  baseUrl = 'https://localhost:5001/api/'
+  private http = inject(HttpClient)
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/notFound').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/unauthorised').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/internalerror').subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
+      next: response => console.log(response),
+      error: error => console.log(error)
+    })
+  }
+}
