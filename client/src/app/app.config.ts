2b91dad9d20aa40df035467857c1c910746ad5d8
Nimira43 <n.rabone72@aol.com>
2026-07-14 10:38:54 +0100

    Creating interceptor for handling API errors.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import 'zone.js'
-
 import { routes } from './app.routes'
-import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
+import { errorInterceptor } from './core/interceptors/error-interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([errorInterceptor]))
   ]
 }
