3fb03b62d8cae9adba65d0efdb710e9130e06768
Nimira43 <n.rabone72@aol.com>
2026-08-02 17:15:34 +0100

    Configuring Redis.

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,12 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 builder.Services.AddScoped<IProductRepository, ProductsRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+  var connString = builder.Configuration.GetConnectionString("Redis") ?? throw new Exception("Cannot get Redis connection string");
+  var configuration = ConfigurationOptions.Parse(connString, true);
+  return ConnectionMultiplexer.Connect(configuration);
+});
 
 var app = builder.Build();
 
