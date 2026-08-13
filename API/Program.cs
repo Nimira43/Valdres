264a7e49bc6aecffc401034ff222d03075c2c0a4
Nimira43 <n.rabone72@aol.com>
2026-08-13 21:43:06 +0100

    CartController

--- API/Program.cs
+++ API/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
   var configuration = ConfigurationOptions.Parse(connString, true);
   return ConnectionMultiplexer.Connect(configuration);
 });
-builder.Services.AddSingleton<ICartService, CartService>()
+builder.Services.AddSingleton<ICartService, CartService>();
 
 var app = builder.Build();
 
