b99db77af820a03a8d0da9179f5e5d331b3010f5
Nimira43 <n.rabone72@aol.com>
2026-08-16 09:44:51 +0100

    Testing the .NET/Redis cart endpoints. Also creating cart component and cart service.

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from '@angular/core'
 import { MatIcon } from '@angular/material/icon'  
 import { MatBadge } from '@angular/material/badge'
-import { RouterLink, RouterLinkActive } from "@angular/router";  
-import { BusyService } from '../../core/services/busy.service';
-import { MatProgressBar } from '@angular/material/progress-bar';
+import { RouterLink, RouterLinkActive } from '@angular/router'  
+import { BusyService } from '../../core/services/busy.service'
+import { MatProgressBar } from '@angular/material/progress-bar'
 
 @Component({
   selector: 'app-header',
