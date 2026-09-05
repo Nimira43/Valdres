011ba989a8504fb0d26ac39aa75742e01ac86123
Nimira43 <n.rabone72@aol.com>
2026-09-05 17:42:52 +0100

    Creating the order totals.

--- client/src/app/shared/components/order-summary/order-summary.component.ts
+++ client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -1,19 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { RouterLink } from '@angular/router'
-import { MatFormField, MatLabel } from '@angular/material/form-field'
-import { MatInput } from '@angular/material/input'
+import { CartService } from '../../../core/services/cart.service'
+import { CurrencyPipe } from '@angular/common'
 
 @Component({
   selector: 'app-order-summary',
   imports: [
     RouterLink,
-    MatFormField,
-    MatLabel,
-    MatInput
+    CurrencyPipe
   ],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.css',
 })
 export class OrderSummaryComponent {
-
+  cartService = inject(CartService)
 }
