0d250deb72a232f07de5370819290e8ec9102409
Nimira43 <n.rabone72@aol.com>
2026-09-15 04:15:34 +0100

    Update cart functionality to the product details.

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -3,12 +3,14 @@ import { Component, inject, OnInit } from '@angular/core'
 import { ShopService } from '../../../core/services/shop.service'
 import { ActivatedRoute } from '@angular/router'
 import { Product } from '../../../shared/models/product'
+import { CartService } from '../../../core/services/cart.service'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
   imports: [
-    CurrencyPipe,
+    CurrencyPipe, FormsModule
 ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css',
@@ -16,7 +18,10 @@ import { Product } from '../../../shared/models/product'
 export class ProductDetailsComponent implements OnInit {
   private shopService = inject(ShopService)
   private activatedRoute = inject(ActivatedRoute)
+  private cartService = inject(CartService)
   product?: Product
+  quantityInCart = 0
+  quantity = 1
 
   ngOnInit(): void {
     this.loadProduct()
@@ -27,8 +32,35 @@ export class ProductDetailsComponent implements OnInit {
     if (!id) return
 
     this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product => {
+        this.product = product
+        this.updateQuantityInCart()
+      },
       error: error => console.log(error)
     })
+  }
+
+  updateCart() {
+    if (!this.product) return
+
+    if (this.quantity > this.quantityInCart) {
+      const itemsToAdd = this.quantity - this.quantityInCart
+      this.quantityInCart += itemsToAdd
+      this.cartService.addItemToCart(this.product, itemsToAdd)
+    } else {
+      const itemsToRemove = this.quantityInCart - this.quantity
+      this.quantityInCart -= itemsToRemove
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove)
+    }
+  }
+
+  updateQuantityInCart() {
+    this.quantityInCart = this.cartService.cart()?.items
+      .find(x => x.productId === this.product?.id)?.quantity || 0
+    this.quantity = this.quantityInCart || 1
+  }
+
+  getButtonText() {
+    return this.quantityInCart > 0 ? 'Update Cart' : 'Add to Cart'
   }
 }
