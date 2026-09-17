3f8061351063888af19ed5f7ff617fb7333442a5
Nimira43 <n.rabone72@aol.com>
2026-09-17 17:37:20 +0100

    Setting up Identity.

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -22,6 +23,9 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
   return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+  .AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 
@@ -31,6 +35,7 @@ app.UseCors(x => x
   .AllowAnyMethod()
   .WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
+app.MapIdentityApi<AppUser>();
 
 try
 {
